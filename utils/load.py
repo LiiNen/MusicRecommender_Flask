18f4119a2407d9940b9c36a4d797158ef6a97d70
LiiNen <kjeonghoon065@gmail.com>
2021-05-25 13:38:20 +0900

    Add requirements.txt for env setup

--- utils/load.py
+++ utils/load.py
@@ -4,11 +4,9 @@ import numpy as np
 import csv
 import sklearn
 from glob import glob
-import spleeter
 from tqdm import tqdm
 import pandas as pd
 from sklearn.cluster import KMeans
-import seaborn as sns
 from scipy.spatial.distance import cdist
 
 data = pd.read_csv('dataset.csv', encoding='cp949')
